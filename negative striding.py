7fb5feb3e6241b01baf7d1152a2a51f7b4d9a640
M-Likhith <likhith.mentrumilli@gmail.com>
2026-03-16 20:14:02 +0530

    Add files via upload

---
+++ negative striding.py
@@ -0,0 +1,12 @@
+Python 3.14.1 (tags/v3.14.1:57e0d17, Dec  2 2025, 14:05:07) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #negative striding
+>>> a="python course"
+>>> a[-1:-8:-3]
+'eu '
+>>> a[-2:-12:-4]
+'sch'
+>>> a[-4:-13:-5]
+'uo'
+>>> a[-3:-9:-2]
+'ro '
